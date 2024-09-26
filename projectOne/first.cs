6dcf522a6e0301d20390b4d41cdd0484a7eb7986
k-ghale <kabinghale888@gmail.com>
2024-09-26 01:47:28 -0400

    added first.cs

---
+++ projectOne/first.cs
@@ -0,0 +1,20 @@
+using System;
+
+class first 
+{
+    static void Main()
+    {
+        int myNum = 5;
+        double myDouble = 5.99D;
+        char myLetter = 'K';
+        bool myBool = true;
+        string mytext = "Hello World";
+
+        Console.WriteLine(myNum);
+        Console.WriteLine(myDouble);
+        Console.WriteLine(myLetter);
+        Console.WriteLine(myBool);
+        Console.WriteLine(mytext);
+    }
+}
+
