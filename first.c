eccd0ae68a1405c16b637ba14e7ca600ca5a47aa
k-ghale <kabinghale888@gmail.com>
2024-09-26 00:50:03 -0400

    added first.c

---
+++ first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  // Include standard input/output library
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!\n");
+    
+    // Return 0 to indicate successful execution
+    return 0;
+}
